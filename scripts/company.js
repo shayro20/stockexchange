f68372dd8a8b15195b00396c413f5caba1860902
shay ruimi <111450912+shayro20@users.noreply.github.com>
2022-11-23 11:45:25 +0200

    done with milestone8&9

--- scripts/company.js
+++ scripts/company.js
@@ -1,79 +1,167 @@
-const webParameter = new URLSearchParams(window.location.search);
-const symbol = webParameter.get("symbol");
-const headLine = document.getElementById("title");
-const describe = document.getElementById("describe");
-const picture = document.getElementById("pic");
-const site = document.getElementById("site");
-const stock = document.getElementById("price");
-const prcntChange = document.getElementById("percentage");
-const urlMe =
-  "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/";
-const historyUrl =
-  "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/historical-price-full/";
-getFace();
-async function getFace() {
-  const res = await fetch(urlMe + symbol);
-  const data = await res.json();
-  console.log(data.profile);
-  const name = data.profile.companyName;
-  const image = data.profile.image;
-  const description = data.profile.description;
-  const link = data.profile.website;
-  const stockprice = data.profile.price;
-  const change = Number(data.profile.changesPercentage);
-  const changeD = change.toFixed(2)
-  headLine.innerHTML = name;
-  picture.src = image;
-  site.href = link;
-  site.innerHTML = `Company Website`;
-  describe.innerHTML = description;
-  stock.innerHTML = `Stock Price: $`+stockprice;
-  if (changeD > 0) {
-    prcntChange.innerHTML = `(+${changeD}` + `%)`;
-    prcntChange.classList.add("text-success");
-  } else {
-    prcntChange.innerHTML = `(${changeD}` + `%)`;
-    prcntChange.classList.add("text-danger");
+class CompanyInfo {
+  constructor(container, symbol) {
+    // div and the symbol from the call
+    this.container = container;
+    this.symbol = symbol;
+    // picture and headline
+    this.picAndHead = document.createElement("div");
+    this.icon = document.createElement("img");
+    this.head = document.createElement("div");
+    // stock price and percentage
+    this.stockContain = document.createElement("div");
+    this.stockPrice = document.createElement("span");
+    this.stockPercent = document.createElement("span");
+    // description
+    this.paragraph = document.createElement("p");
+    // website link
+    this.linkContainer = document.createElement("div");
+    this.linkBtn = document.createElement("button");
+    this.webLink = document.createElement("a");
+    // chart
+    this.chartContainer = document.createElement("div");
+    this.chartCanvas = document.createElement("canvas");
+    //spinner
+    this.spinner = document.createElement("div");
+    this.spinnerSpan = document.createElement("span");
+
+    this.urlMe =
+      "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/";
+    this.historyUrl =
+      "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/historical-price-full/";
   }
-}
-
-async function histroyFetch() {
-  const response = await fetch(historyUrl + symbol + `?serietype=line`);
-  const historyData = await response.json();
-  const history = historyData.historical;
-  console.log(historyData);
-  setTimeout(() => {chart(history);
-    
-  }, 2000);
-}
-histroyFetch();
-
-function chart(arr) {
-  const labels = [];
-
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: "Stock Price History",
-        fill: 'origin',
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        data: [],
-      },
-    ],
-  };
-  const scale = data.datasets[0].data;
-  for (let i = 0; i < arr.length; i++) {
-    labels.push(arr[i].date);
-    scale.push(arr[i].close);
+  load() {
+    this.setHeader();
+    this.setStock();
+    this.setParagraph();
+    this.setWebLink();
+    this.setChart();
+    this.setSpinner();
+    this.appendIt();
+  }
+  // picture and headline
+  setHeader() {
+    this.picAndHead.classList = "d-flex justify-content-start mb-5";
+    this.icon.id = "pic";
+    this.head.classList = "text-center display-4 ms-5";
+    this.head.id = "title";
+    this.picAndHead.append(this.icon);
+    this.picAndHead.append(this.head);
+  }
+  // stock price and percentage
+  setStock() {
+    this.stockContain.classList = "fs-3 mb-5";
+    this.stockPrice.id = "price";
+    this.stockPercent.id = "percentage";
+    this.stockContain.append(this.stockPrice);
+    this.stockContain.append(this.stockPercent);
+  }
+  // description
+  setParagraph() {
+    this.paragraph.classList = "fs-5";
+    this.paragraph.id = "describe";
+  }
+  // website link
+  setWebLink() {
+    this.linkContainer.classList = "d-flex justify-content-center";
+    this.linkBtn.type = "button";
+    this.linkBtn.classList = "btn btn-info";
+    this.webLink.classList =
+      "text-decoration-none text-center text-light border border-info fs-5";
+    this.webLink.id = "site";
+    this.linkBtn.append(this.webLink);
+    this.linkContainer.append(this.linkBtn);
+  }
+  // chart
+  setChart() {
+    this.chartCanvas.id = "myChart";
+    this.chartContainer.append(this.chartCanvas);
+  }
+  //spinner
+  setSpinner() {
+    this.spinner.id = "spinner-two";
+    this.spinner.classList = "d-block m-auto spinner-grow text-info";
+    this.spinner.role = "status";
+    this.spinnerSpan.classList = "visually-hidden";
+    this.spinnerSpan.innerText = "Loading...";
+    this.spinner.append(this.spinnerSpan);
+  }
+  appendIt() {
+    this.container.append(this.picAndHead);
+    this.container.append(this.stockContain);
+    this.container.append(this.paragraph);
+    this.container.append(this.linkContainer);
+    this.container.append(this.chartContainer);
+    this.container.append(this.spinner);
+    this.getFace();
   }
 
-  const config = {
-    type: "line",
-    data: data,
-    options: {},
-  };
-  const myChart = new Chart(document.getElementById("myChart"), config);
-  document.getElementById("spinner-two").classList.add("visually-hidden")
+  // -----FETCH--------------------------------
+  async getFace() {
+    const res = await fetch(this.urlMe + this.symbol);
+    const data = await res.json();
+    const {
+      companyName: name,
+      image,
+      description,
+      website: link,
+      price: stockPrice,
+    } = data.profile;
+    const change = Number(data.profile.changesPercentage);
+    const changeD = change.toFixed(2);
+    this.head.innerHTML = name;
+    this.icon.src = image;
+    this.webLink.href = link;
+    this.webLink.innerHTML = `Company Website`;
+    this.paragraph.innerHTML = description;
+    this.stockPrice.innerHTML = `Stock Price: $` + stockPrice;
+    if (changeD > 0) {
+      this.stockPercent.innerHTML = `(+${changeD}` + `%)`;
+      this.stockPercent.classList.add("text-success");
+    } else {
+      this.stockPercent.innerHTML = `(${changeD}` + `%)`;
+      this.stockPercent.classList.add("text-danger");
+    }
+  }
+
+  // // -------CHART------------------------------------------------
+
+  async addChart() {
+    const response = await fetch(
+      this.historyUrl + this.symbol + `?serietype=line`
+    );
+    const historyData = await response.json();
+    const history = historyData.historical;
+    setTimeout(() => {
+      this.chart(history);
+    }, 1500);
+  }
+  chart(arr) {
+    const labels = [];
+
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "Stock Price History",
+          fill: "origin",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: [],
+        },
+      ],
+    };
+    const scale = data.datasets[0].data;
+    for (let i = 0; i < arr.length; i++) {
+      labels.push(arr[i].date);
+      scale.push(arr[i].close);
+    }
+
+    const config = {
+      type: "line",
+      data: data,
+      options: {},
+    };
+    const myChart = new Chart(document.getElementById("myChart"), config);
+    this.spinner.classList.add("visually-hidden");
+  }
 }
